eaf531f679e9ae3dfce84288aff2b71b4f8a96bc
awin <mcmangunay@paterostechnologicalcollege.edu.ph>
2025-03-19 15:35:37 +0800

    added divide method for group 4

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public static int divide(int a, int b) {
+    if (b == 0) {
+        System.out.println("Error: Division by zero is not allowed.");
+        return 0; // 
+    }
+    return a / b;
+}
